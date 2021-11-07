344426a1fef5adea9474bd35e401157c2b418bcb
Pierre-Sylvain Augereau <ps.augereau@gmail.com>
2021-11-07 12:55:22 +0100

    chore (base): Update config

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "b3e4cf5d-0dfc-426c-a3b6-3a031f3fc645")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ceciestunmot2passe>")
+    APP_ID = os.environ.get("MicrosoftAppId", "851864ee-9c18-44fc-9902-a43f474ac040")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ceciestunmot2passe")
     LUIS_APP_ID = os.environ.get("LuisAppId", "4d592d65-9c86-4987-a131-c73f66f26f55")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "06517d0c606c4cafb0ab434f6ff14bfc")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
@@ -22,3 +22,5 @@ class DefaultConfig:
     APPINSIGHTS_INSTRUMENTATION = os.environ.get(
         "AppInsightsInstrumentationCnx", "InstrumentationKey=3cb89e6d-14fa-4d3b-ab8f-aec8200fd004;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/"
     )
+    # https://flyme26.azurewebsites.net/api/messages
+    # az ad app create --display-name "flyme26" --password "Ceciestunmot2passe" --available-to-other-tenants
