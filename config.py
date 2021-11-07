0e11d6efd70d5406088fa769374a2f75498b5866
Pierre-Sylvain Augereau <ps.augereau@gmail.com>
2021-11-07 20:02:05 +0100

    chore (base): update config

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "851864ee-9c18-44fc-9902-a43f474ac040")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ceciestunmot2passe")
+    APP_ID = os.environ.get("MicrosoftAppId", "7ff0aa79-e256-4375-a39c-d0aeb166c51e")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "-(Ia:z98=JHyNv=UwL>*Z]2h8h&")
     LUIS_APP_ID = os.environ.get("LuisAppId", "4d592d65-9c86-4987-a131-c73f66f26f55")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "06517d0c606c4cafb0ab434f6ff14bfc")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
