919aa1bcb9bd8d0cd20b65b02402652ebc83cdfe
Pierre-Sylvain Augereau <ps.augereau@gmail.com>
2021-11-07 11:36:57 +0100

    chore (base): Update config

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "426d7f96-c527-4de8-9146-18e6a9b8ef47")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "CeciEstUnMot2PasseAssezLong")
+    APP_ID = os.environ.get("MicrosoftAppId", "b3e4cf5d-0dfc-426c-a3b6-3a031f3fc645")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ceciestunmot2passe>")
     LUIS_APP_ID = os.environ.get("LuisAppId", "4d592d65-9c86-4987-a131-c73f66f26f55")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "06517d0c606c4cafb0ab434f6ff14bfc")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westeurope.api.cognitive.microsoft.com")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey", "99aff7b0-2faa-4bb2-86c1-b8f6ee7cbf34"
+        "AppInsightsInstrumentationKey", "3cb89e6d-14fa-4d3b-ab8f-aec8200fd004"
     )
     APPINSIGHTS_INSTRUMENTATION = os.environ.get(
-        "AppInsightsInstrumentationCnx", "InstrumentationKey=99aff7b0-2faa-4bb2-86c1-b8f6ee7cbf34;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/"
+        "AppInsightsInstrumentationCnx", "InstrumentationKey=3cb89e6d-14fa-4d3b-ab8f-aec8200fd004;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/"
     )
